24f3478c053cd540ddcae59b39adc773495e3643
brunadionisio5 <brunadionisio398@gmail.com>
2025-05-01 12:03:54 -0300

    Desafio concluido

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,9 @@
-
 import { useState } from 'react';
 import gitLogo from '../assets/github.png'
 import Input from '../components/Input';
 import Button from '../components/Button';
 import ItemRepo from '../components/ItemRepo';
 import { api } from '../services/api';
-
 import { Container } from './styles';
 
 function App() {
@@ -13,15 +11,12 @@ function App() {
   const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
 
-
   const handleSearchRepo = async () => {
 
     const {data} = await api.get(`repos/${currentRepo}`)
-
     if(data.id){
 
       const isExist = repos.find(repo => repo.id === data.id);
-
       if(!isExist){
         setRepos(prev => [...prev, data]);
         setCurrentRepo('')
@@ -35,10 +30,8 @@ function App() {
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
-
-    // utilizar filter.
+    setRepos(prevRepos => prevRepos.filter(repo => repo.id !== id));
   }
-
 
   return (
     <Container>
@@ -48,6 +41,7 @@ function App() {
       {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
     </Container>
   );
+  
 }
 
 export default App;
